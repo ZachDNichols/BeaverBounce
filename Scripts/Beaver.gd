d590ad5072d305adf9f742a73a8befa622d1a86a
Zach Nichols <zachdnichols@gmail.com>
2023-10-02 13:42:00 -0500

    Camera follows beaver, sprite flips

--- Scripts/Beaver.gd
+++ Scripts/Beaver.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -800.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -14,12 +14,18 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 	else:
 		velocity.y = JUMP_VELOCITY
+		
+	$"../Camera2D".position.y = $".".position.y
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		if direction > 0:
+			$Sprite2D.flip_h = false
+		elif direction < 0:
+			$Sprite2D.flip_h = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
