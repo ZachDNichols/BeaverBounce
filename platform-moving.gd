e6d6f946b7dd351db7a2de61882edda4705512b2
yengcheel <yengchee2011@hotmail.com>
2023-10-08 20:52:14 -0500

    Added temporary platform
    Needs final models and finished code.

---
+++ platform-moving.gd
@@ -0,0 +1,21 @@
+extends AnimatableBody2D
+
+
+
+@export var offset = Vector2(0,-320)
+
+@export var duration = 5.0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	start_tween()
+
+func start_tween():
+	var tween = get_tree().create_tween().set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
+	tween.set_loops().set_parallel(false)
+	tween.tween_property($AnimatableBody2d, "position", offset, duration / 2)
+	tween.tween_property($AnimatableBody2d, "position", Vector2.ZERO, duration / 2)
+	
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
