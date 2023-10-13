d0b9ece6df7e6916eb3910a806a9aa9baa66ec7a
Zach Nichols <zachdnichols@gmail.com>
2023-10-13 09:39:05 -0500

    Updated beaver sprite

--- Scripts/Beaver.gd
+++ Scripts/Beaver.gd
@@ -4,7 +4,7 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -800.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-@onready var sprite =  $AnimatedSprite2D
+@onready var sprite = $Beaver
 
 func _physics_process(delta):
 	# Add the gravity.
